52e787c360f80023b07186e0a7f98e207429c96d
4teen Genesis <info14fourteen@gmail.com>
2026-03-24 08:16:10 +0500

    Update referral.ts

--- shared/config/referral.ts
+++ shared/config/referral.ts
@@ -1,1 +1,10 @@
+export const REFERRAL_QUERY_PARAM = "r";
+export const REFERRAL_STORAGE_KEY = "fourteen_referral_first_touch_v1";
 
+export const REFERRAL_TTL_DAYS = 30;
+export const REFERRAL_TTL_MS = REFERRAL_TTL_DAYS * 24 * 60 * 60 * 1000;
+
+export const REFERRAL_SLUG_MIN_LENGTH = 3;
+export const REFERRAL_SLUG_MAX_LENGTH = 64;
+
+export const REFERRAL_SLUG_PATTERN = /^[a-z0-9](?:[a-z0-9-]{1,62}[a-z0-9])?$/;
